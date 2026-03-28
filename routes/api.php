770f3461f963b7d2bc307716c6e6dc6b03697f24
kota <gantzpocky2221@gmail.com>
2026-03-28 16:35:01 +0900

    api.phpに追記

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/test-cors', function () {
+    return response()->json(['message' => 'URLは合ってる']);
+});
