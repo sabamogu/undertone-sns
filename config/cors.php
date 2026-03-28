e14314e034aa7404e37076879bbb2bc459e11c69
kota <gantzpocky2221@gmail.com>
2026-03-28 15:50:57 +0900

    cors.phpファイルの追加、ドメイン制限設定

---
+++ config/cors.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+    // 1. 許可するパス（URLのパターン）
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    // 2. 許可するメソッド（GET, POST, etc.）
+    'allowed_methods' => ['*'],
+
+    // 3. 許可するオリジン（アクセス元のドメイン）
+    'allowed_origins' => [
+    'https://undertone-production.up.railway.app',  // 本番の自分のドメイン
+    'http://localhost',     // ローカル開発用（これを忘れると自分のPCで動かなくなる）
+    ],
+    
+    'allowed_origins_patterns' => [],
+
+    // 4. 許可するヘッダー（Content-Type など）
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    // 5. キャッシュの有効期限
+    'max_age' => 0,
+
+    // 6. クッキーや認証情報の送信を許可するか
+    'supports_credentials' => false,
+
+];
