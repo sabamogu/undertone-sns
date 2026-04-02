19529ac74b8d15c4fae995a2e7f16c14e5423139
kota <gantzpocky2221@gmail.com>
2026-04-02 15:59:36 +0900

    テストコードをapi.phpに追記

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,13 @@ Route::get('/user', function (Request $request) {
 Route::get('/test-cors', function () {
     return response()->json(['message' => 'URLは合ってる']);
 });
+
+Route::post('/contact', function (Request $request) {
+    // バリデーションチェック（わざとエラーを起こすため）
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email',
+    ]);
+
+    return response()->json(['message' => '送信成功！']);
+});
